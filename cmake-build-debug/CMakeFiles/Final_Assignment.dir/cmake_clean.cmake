17c19cfb5675c8dd03a3a3becaae1958118146d1
Petter Vang Brakalsvalet <44548421+PetterBraka@users.noreply.github.com>
2019-02-16 15:04:50 +0000

    Add files via upload

---
+++ cmake-build-debug/CMakeFiles/Final_Assignment.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Final_Assignment.dir/main.c.o"
+  "CMakeFiles/Final_Assignment.dir/functions.c.o"
+  "Final_Assignment.pdb"
+  "Final_Assignment.exe"
+  "libFinal_Assignment.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Final_Assignment.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
